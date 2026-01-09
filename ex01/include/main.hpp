cf5cb8bacf0f3413013301c2aa4a9554b0f02627
dnnyll <leal47danny@gmail.com>
2026-01-09 17:14:36 +0100

    ex01 work in progress

--- ex01/include/main.hpp
+++ ex01/include/main.hpp
@@ -1,21 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   main.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daniefe2 <daniefe2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/06 15:36:53 by daniefe2          #+#    #+#             */
-/*   Updated: 2026/01/09 16:47:17 by daniefe2         ###   ########.fr       */
+/*   Created: 2026/01/09 16:49:37 by daniefe2          #+#    #+#             */
+/*   Updated: 2026/01/09 16:51:02 by daniefe2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
 #include "phonebook.hpp"
 
-std::string	phonebook::get_first_name(int id)
-{
-	return (contacts[id].get_first_name());
-}
-
-
-
+#endif
--- ex01/include/phonebook.hpp
+++ ex01/include/phonebook.hpp
@@ -6,14 +6,25 @@
 /*   By: daniefe2 <daniefe2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 12:39:28 by daniefe2          #+#    #+#             */
-/*   Updated: 2026/01/09 13:30:03 by daniefe2         ###   ########.fr       */
+/*   Updated: 2026/01/09 16:48:21 by daniefe2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
+
 #include <iostream>
 #include <cctype>
 #include <string>
+
+class phonebook
+{
+private:
+	// contact	contacts[id];
+	int		index;
+	int		completed;
+public:
+
+};
 
 #endif
