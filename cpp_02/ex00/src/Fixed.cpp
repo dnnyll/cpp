ac1d86d7c4ce129a444030af071dbb0bc3959c83
dnnyll <leal47danny@gmail.com>
2026-02-03 12:41:26 +0100

    cpp02_ex00 completed

---
+++ cpp_02/ex00/src/Fixed.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniefe2 <daniefe2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/03 09:52:34 by daniefe2          #+#    #+#             */
+/*   Updated: 2026/02/03 12:40:40 by daniefe2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+//	default constructor
+Fixed::Fixed() : _rawBits(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+//	copy constructor
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+//	copy assignment operator
+//	note:	=operator= is both the function name and the mechanism that defines
+//	how the equal sign behaves for your class.
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		this->_rawBits = other.getRawBits();
+	return (*this);
+}
+
+//	default constructor
+Fixed::~Fixed()
+{
+	std::cout << "Default destructor called" << std::endl;
+}
+
+//	get function
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_rawBits);
+}
+
+//	set function
+void	Fixed::setRawBits(int const raw)
+{
+	this->_rawBits = raw;
+}
---
+++ cpp_02/ex00/src/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniefe2 <daniefe2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/03 09:42:41 by daniefe2          #+#    #+#             */
+/*   Updated: 2026/02/03 12:40:31 by daniefe2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Fixed.hpp"
+
+int	main( void )
+{
+	Fixed a;		// Default constructor called → a._rawBits = 0
+	Fixed b(a);		// Copy constructor called    → b._rawBits = a._rawBits
+	Fixed c;		// Default constructor called → c._rawBits = 0
+
+	c = b;			// Copy assignment operator called → this = c, other = b, copies _rawBits
+
+	/*	when the code hits this equal sign, it will look and call the
+	//	function: Fixed &Fixed::operator=(const Fixed &other)
+	//	within this function, the keyword "this" will get the value
+	//	stored in c and other will get the value stored in b
+	//	it will then check if this != &other to avoid self-assignment
+	//	and copy the internal _rawBits from other into this
+	//	finally it will return *this so that chained assignments would work */
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return (0);
+}
