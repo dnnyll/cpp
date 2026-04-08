f578df4144049b51e03beb2088cf90704d0c23ef
dnnyll <leal47danny@gmail.com>
2026-04-08 11:26:49 +0200

    working on cpp_08, ex01 and ex02 need main tests:

--- cpp_08/ex02/inc/MutantStack.hpp
+++ cpp_08/ex02/inc/MutantStack.hpp
@@ -1,18 +1,94 @@
 #ifndef		MUTANTSTACK_HPP
 #define		MUTANTSTACK_HPP
 
+#include	<stack>
+#include	<iostream>
 
-class	MutantStack
+//	inheriting from stack allows us to access the protected variable c
+//	which can be found in stl_stack.h (line 146)
+//	it also allows to inherit from all functions it contains
+
+template <typename T>
+class	MutantStack : public std::stack<T>
 {
-	private:
-
-		std::stack	internal_stack;
-
 	public:
 
-		MutantStack();
-		~MutantStack()
+		//	type definitions
+		typedef typename	std::stack<T>::container_type::iterator iterator;
+		typedef typename	std::stack<T>::container_type::const_iterator const_iterator;
 
+		//	OCF
+		MutantStack();
+		MutantStack(const MutantStack &copy);
+		MutantStack	&operator=(const MutantStack &copy);
+		~MutantStack();
+
+		//	iterators
+		iterator		begin();
+		iterator		end();
+		const_iterator	begin() const;
+		const_iterator	end() const;
 };
+
+//	OCF
+//	default constructor
+template <typename T>
+MutantStack<T>::MutantStack() : std::stack<T>()
+{
+	std::cout << "[MutantStack]\tdefault constructor called" << std::endl;
+}
+
+//	copy constructor
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack<T> &copy) : std::stack<T>(copy)
+{
+	std::cout << "[MutantStack]\tcopy constructor called" << std::endl;
+}
+
+//	copy assignment operator
+template <typename T>
+MutantStack<T>	&MutantStack<T>::operator=(const MutantStack<T> &copy)
+{
+	std::cout << "[MutantStack]\tcopy assignment operator called" << std::endl;
+	if (this != &copy)
+	{
+		std::stack<T>::operator=(copy);
+	}
+	return (*this);
+}
+
+//	destructor
+template <typename T>
+MutantStack<T>::~MutantStack()
+{
+	std::cout << "[MutantStack]\tdestructor called" << std::endl;
+}
+
+
+
+//	iterators
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+	return (this->c.begin());
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+	return (this->c.end());
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::begin() const
+{
+	return (this->c.begin());
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::end() const
+{
+	return (this->c.end());
+}
 
 #endif
--- cpp_08/ex02/src/MutantStack.cpp
+++
@@ -1,2 +0,0 @@
-#include	"../inc/MutantStack.hpp"
-
